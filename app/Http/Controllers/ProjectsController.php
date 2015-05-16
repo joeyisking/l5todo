a264347832b8b39faf34d1103bc6e065954e5f2b
Joseph <wma_10@hotmail.com>
2015-05-16 13:07:20 +1000

    adding views and updating controllers

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -1,22 +1,24 @@
 <?php namespace App\Http\Controllers;
-
+ 
+use App\Project;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+ 
 use Illuminate\Http\Request;
-
+ 
 class ProjectsController extends Controller {
-
+ 
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
 	public function index()
-	{	
-		return view('projects.index');
+	{
+		$projects = Project::all();
+		return view('projects.index', compact('projects'));
 	}
-
+ 
 	/**
 	 * Show the form for creating a new resource.
 	 *
@@ -24,9 +26,9 @@ class ProjectsController extends Controller {
 	 */
 	public function create()
 	{
-		//
+		return view('projects.create');
 	}
-
+ 
 	/**
 	 * Store a newly created resource in storage.
 	 *
@@ -36,49 +38,49 @@ class ProjectsController extends Controller {
 	{
 		//
 	}
-
+ 
 	/**
 	 * Display the specified resource.
 	 *
-	 * @param  int  $id
+	 * @param  \App\Project $project
 	 * @return Response
 	 */
-	public function show($id)
+	public function show(Project $project)
 	{
-		//
+		return view('projects.show', compact('project'));
 	}
-
+ 
 	/**
 	 * Show the form for editing the specified resource.
 	 *
-	 * @param  int  $id
+	 * @param  \App\Project $project
 	 * @return Response
 	 */
-	public function edit($id)
+	public function edit(Project $project)
 	{
-		//
+		return view('projects.edit', compact('project'));
 	}
-
+ 
 	/**
 	 * Update the specified resource in storage.
 	 *
-	 * @param  int  $id
+	 * @param  \App\Project $project
 	 * @return Response
 	 */
-	public function update($id)
+	public function update(Project $project)
 	{
 		//
 	}
-
+ 
 	/**
 	 * Remove the specified resource from storage.
 	 *
-	 * @param  int  $id
+	 * @param  \App\Project $project
 	 * @return Response
 	 */
-	public function destroy($id)
+	public function destroy(Project $project)
 	{
 		//
 	}
-
+ 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,20 +13,15 @@
 
 Route::get('/', 'WelcomeController@index');
 
-//Route::get('home', 'HomeController@index');
-
-//Route::controllers([
-//	'auth' => 'Auth\AuthController',
-//	'password' => 'Auth\PasswordController',
-//]);
-
-Route::resource('projects', 'ProjectsController');
-//Route::resource('tasks', 'TasksController');
-Route::resource('project.tasks', 'TasksController');
-
+// Provide controller methods with object instead of ID
+Route::model('tasks', 'Task');Route::model('projects', 'Project'); 
+// Use slugs rather than IDs in URLs
 Route::bind('tasks', function($value, $route) {
 	return App\Task::whereSlug($value)->first();
 });
 Route::bind('projects', function($value, $route) {
 	return App\Project::whereSlug($value)->first();
 });
+ 
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
