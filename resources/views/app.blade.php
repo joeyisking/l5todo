2b0d2567c938c293b5f1a42b5666a2fa5feb938f
Joseph <wma_10@hotmail.com>
2015-08-15 14:08:47 +1000

    onscreen editor

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -71,8 +71,9 @@
 	</div>
 
 	<!-- Scripts -->
+
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-
+    <script src="./js/formatter.js"></script>
 </body>
 </html>
